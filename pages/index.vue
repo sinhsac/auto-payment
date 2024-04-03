8348b8691ba44977bd77ba32e0f1272ce3c7c17e
Sac Le <sacls@superman.com>
2024-04-03 22:40:12 +0700

    update config

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
-const { data } = await useFetch('/api/hello')
+const { data } = await useFetch('/api/products')
 </script>
 
 <template>
-  <pre>{{ data }}</pre>
+  <div v-for="p in data.products">
+    <div>{{p.item.item_id}}</div>
+    <div>{{p.item.title}}</div>
+    <div>{{p.item.price}}</div>
+  </div>
+  <pre>
+    {{data.products}}
+  </pre>
 </template>
+
+<style scoped>
+
+</style>
